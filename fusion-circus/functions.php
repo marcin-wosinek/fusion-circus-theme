6db78da62627bf0db3b2381cd5a465a3bb407387
Marcin Wosinek <marcin.wosinek@gmail.com>
2026-07-26 11:32:45 +0200

    Scaffold fusion-circus child theme

---
+++ fusion-circus/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style(
+		'fusion-circus-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
+} );
